6bc1bd756a650905149e95ab9b5491b419b1a775
soufianeid <soufiane.idbrahim@hotmail.com>
2016-06-08 19:00:09 +0000

    First Commit

---
+++ springQuartz/src/main/java/com/example/MyJob.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import com.service.IService;
+
+@Component("metier")
+@DisallowConcurrentExecution
+public class MyJob extends QuartzJobBean{
+
+	private IService service;
+	
+	public void setService(IService service) {
+		this.service = service;
+	}
+
+
+	private String someParam;
+    private int someParam2;
+
+    public void setSomeParam(String someParam) {
+        this.someParam = someParam;
+    }
+
+    public void setSomeParam2(int someParam2) {
+        this.someParam2 = someParam2;
+    }
+
+
+	@Override
+	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+		SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+	    service.sayHello();
+		System.out.println("My job is running with "+someParam+' '+someParam2);
+	}
+}
